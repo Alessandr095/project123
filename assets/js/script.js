5e491b6db4e1323072fc4052476088b2b65a8cad
Alessandro De Siena <a.desiena@icloud.com>
2024-09-25 16:02:57 +0000

    add code to increment right answer and wrong answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,8 +36,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert('Hey you got it right');
+        incrementScore();
     } else {
         alert(`You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -54,9 +56,13 @@ function caculateCorrectAnswer() {
     }
 }
 function incrementScore() {
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 
 }
 function incrementWrongAnswer(){
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 
 }
 function displayAdditionQuestion(operand1, operand2) {
