84fba3d4888d9294cdac2bb496524b3ad389bedb
Alessandro De Siena <a.desiena@icloud.com>
2024-09-25 16:13:03 +0000

    add multiplication game to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,7 +23,9 @@ function runGame(gameType) {
 
     if (gameType === 'addition') {
         displayAdditionQuestion(num1, num2);
-    } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    }else {
         alert(`Unknown game type: ${gameType}`);
         throw(`Unknow game type: ${gameType} abort!`)
     }
@@ -49,7 +51,9 @@ function caculateCorrectAnswer() {
     let operator = document.getElementById('operator').innerText;
 
     if (operator === '+') {
-        return[operand1 + operand2, 'addition'];
+        return [operand1 + operand2, 'addition'];
+    } else if (operator === 'x') {
+        return [operand1 * operand2, 'multiply'];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Abort!`;
@@ -74,6 +78,8 @@ function displayAdditionQuestion(operand1, operand2) {
 function displaySubtractQuestion() {
 
 }
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = 'x'
 }
